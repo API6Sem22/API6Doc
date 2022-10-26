fda59cdf13f35d66912707dac33d61b47c6bfa21
Gabriel Henrique de Souza Ferraz <bi.henriquesf@hotmail.com>
2022-10-25 22:26:58 -0300

    Column datetime

--- Data Warehouse Documentation/DW_D-End-DDL.sql
+++ Data Warehouse Documentation/DW_D-End-DDL.sql
@@ -2,13 +2,13 @@ CREATE TABLE Dim_Cliente (
     cli_id int IDENTITY(1, 1),
     cli_id_ori varchar(250),
     cli_nome varchar(250),
-    cli_data_nascimento varchar(30),
+    cli_data_nascimento datetime,
     cli_qtd_dependente int,
     cli_marca_otica bigint,
     cli_marca_otica_odonto bigint,
     cli_situacao varchar(50),
-    cli_data_cancelamento varchar(30),
-    cli_data_situacao varchar(30),
+    cli_data_cancelamento datetime,
+    cli_data_situacao datetime,
     cli_cod_contrato numeric(18,0),
     CONSTRAINT Dim_Cliente_pk PRIMARY KEY  (cli_id)
 );
@@ -17,7 +17,7 @@ CREATE TABLE Dim_Convenio (
     con_id int IDENTITY(1, 1),
     con_codigo bigint,
     con_nome varchar(250),
-    con_data_suspensao varchar(30),
+    con_data_suspensao datetime,
     CONSTRAINT Dim_Convenio_pk PRIMARY KEY  (con_id)
 );
 
@@ -26,15 +26,15 @@ CREATE TABLE Dim_Plano (
     pln_codigo varchar(50),
     pln_nome varchar(250),
     pln_operadora varchar(250),
-    pln_ini_vigencia varchar(30),
+    pln_ini_vigencia datetime,
     CONSTRAINT Dim_Plano_pk PRIMARY KEY  (pln_id)
 );
 
 CREATE TABLE Dim_Repasse (
     rep_id int IDENTITY(1, 1),
     rep_saude_orig numeric(8,2),
-    rep_competencia varchar(30),
-    rep_data_geracao varchar(30),
+    rep_competencia datetime,
+    rep_data_geracao datetime,
     rep_valor numeric(8,2),
     rep_tipo_beneficiario varchar(5),
     rep_rubrica varchar(60),
@@ -66,5 +66,6 @@ ALTER TABLE Fato_Medical ADD CONSTRAINT fk_plano_medical_pln_id
 ALTER TABLE Fato_Medical ADD CONSTRAINT fk_repasse_medical_rep_id
     FOREIGN KEY (Dim_Repasse_rep_id)
     REFERENCES Dim_Repasse (rep_id);
+
 
 
